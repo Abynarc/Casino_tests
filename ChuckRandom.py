a9ebe2286570e39a7c372853ef9d13fb4c5a924b
Ilia Baranov <114430141+Abynarc@users.noreply.github.com>
2024-08-08 12:45:01 +0300

    Update ChuckRandom.py

--- ChuckRandom.py
+++ ChuckRandom.py
@@ -14,6 +14,16 @@ class TestNewJoke:
             print('Ошибка. Статус код не равен 200')
         result.encoding = 'utf-8'
         print(result.text)
+        check  = result.json()
+        # check_info = check.get ('categories')
+        # assert check_info == []
+        # print('Ответ содержит Категорию')
+        check_value = check.get ('value')
+        name = "Chuck"
+        if name in check_value:
+            print("Ответ содержит шутку со словом CHUCK")
+        else:
+            print ("В ответе не содержится шутки со словом CHUCK")
 
 
 random_joke = TestNewJoke()
